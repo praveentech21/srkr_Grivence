231fd71c82b2b262c222090900b35a5fc8f42755
Sai Praveen <ravikumar_csd@srkrec.edu.in>
2023-08-10 10:48:31 +0530

    All done grivence done shiva

---
+++ changegrievencestatus.php
@@ -0,0 +1,14 @@
+<?php
+
+include 'connect.php';
+
+if(isset($_GET['stugid'])){
+    $changestatus = mysqli_query($conn,"UPDATE `studentgrievence` SET `status` = '1' WHERE `gid` = '{$_GET['stugid']}'");
+    header("location:grievenceadmin.php");
+}
+if(isset($_GET['staffgid'])){
+    $changestatus = mysqli_query($conn,"UPDATE `staffgrievence` SET `status` = '1' WHERE `gid` = '{$_GET['staffgid']}' ");
+    header("location:grievenceadmin.php");
+}
+
+?>
