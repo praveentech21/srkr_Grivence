e34af2940e5c747b4531ee2705bc98b6df981b22
Sai Praveen <ravikumar_csd@srkrec.edu.in>
2023-08-10 10:47:46 +0530

    fotter updated shiva

---
+++ footer.php
@@ -0,0 +1,110 @@
+<!-- Footer -->
+<footer id="page-footer">
+    <section id="footer-top">
+        <div class="container">
+            <div class="footer-inner">
+                <div class="footer-social">
+                    <figure>Follow us:</figure>
+                    <div class="icons">
+                        <a href="https://www.facebook.com/SRKRECOFFICIAL" target='_new'><i class="fa fa-facebook"></i></a>
+						<a href="https://www.instagram.com/srkr_engineering_college" target='_new'><i class="fa fa-instagram"></i></a>
+						<a href="https://twitter.com/SRKR_EC" target='_new'><i class="fa fa-twitter"></i></a>
+                        <a href="https://www.youtube.com/c/SRKREC" target='_new'><i class="fa fa-youtube-play"></i></a>
+                    </div><!-- /.icons -->
+                </div><!-- /.social -->
+<!--			     <div class="search pull-right">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                    <span class="input-group-btn">
+                        <button type="submit" class="btn"><i class="fa fa-search"></i></button>
+                    </span>
+                    </div>
+                </div>
+            </div>/.footer-inner -->
+        </div><!-- /.container -->
+    </section><!-- /#footer-top -->
+
+    <section id="footer-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-3">
+                    <aside>
+                        <header><h4>About SRKR</h4></header>
+                        <p align='justify'>Sagi Rama Krishnam Raju Engineering College (SRKREC), established in 1980, is a self-financed academic institution of coeducation striving to provide a high quality technical education to engineering aspirants. Being one of the premier and well-established technical institutions of the country, it continues to render service to the nation and the world at large with its alumni holding highly prestigious positions and making substantial contribution. 
+                        </p>
+                        <div>
+                            <a href="college_profile.php" class="read-more">View More</a>
+                        </div>
+                    </aside>
+                </div><!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-3">
+                    <aside>
+                        <header><h4>Important Links</h4></header>
+                        <ul class="list-links">
+                            <li><a href="faculty.php">Faculty List</a></li>
+                            <li><a href="infrastructure.php">Infrastructure</a></li>
+                            <li><a href="departments.php">Departments</a></li>
+                            <li><a href="web_mail.php">Web-Mail</a></li>
+                            <li><a href="calendar.php">Events Calendar</a></li>
+                            <li><a href="grievance.php" target='_new'>Online Grievance</a></li> 
+							<li><a href="files/RTI_Declaration.pdf" target='_new'>RTI Declaration</a></li>
+                            <li><a href="contactus.php">Contact Us</a></li>
+                        </ul>
+                    </aside>
+                    </aside>
+                </div><!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-3">
+                    <aside>
+                        <header><h4>Downloads</h4></header>
+                        <ul class="list-links">
+                            <li><a href="ugc.php">UGC Undertaking</a></li>
+                            <li><a href="audit.php">Audited Statements</a></li>
+                            <li><a href="statutes.php">Statutes</a></li>
+                            <li><a href="aqar.php">AQAR</a></li>
+                            <li><a href="best_practices.php">Best Practices of the Institution</a></li>
+                            <li><a href="organogram.php">Organogram</a></li>
+                            <li><a href="distinctiveness.php">Institute’s Distinctiveness</a></li>
+                            <li><a href="strategic_plan.php">Strategic Plan &amp; Deployment</a></li>
+                            <li><a href="feedback_curriculum.php">Feedback on Curriculum</a></li>
+                            <li><a href="gender_sensitisation_plan.php">Annual Gender Sensitisation Plan</a></li>
+                        </ul>
+                    </aside>
+                </div><!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-3">
+                    <aside>
+							                        <header><h4>Contact Us</h4></header>
+                        <address>
+                            <strong>SRKR Engineering College</strong>
+                            <br>
+                            <span>(Autonomous)</span>
+                            <br><br>
+                            <span>SRKR Marg, China Amiram, </span>
+                            <br>
+                            <span>Bhimavaram, A.P, India - 534204</span>
+                            <br>
+                            <abbr title="Telephone">Telephone:</abbr> +91 (8816) 223332
+                            <br>
+							<abbr title="Telephone">Mobile:</abbr> +91 9848823332, 9848381818<br>
+                            <abbr title="Email">Email:</abbr> <a href="#">principal@srkrec.ac.in</a>
+                        </address>
+						<img src="assets/img/logo-white.png">
+
+                    </aside>
+                </div><!-- /.col-md-3 -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+        <div class="background"><img src="assets/img/background-city.png" class="" alt=""></div>
+    </section><!-- /#footer-content -->
+
+    <section id="footer-bottom">
+        <div class="container">
+            <div class="footer-inner">
+                    <div class="copyright"><a href='web_team.php'><b style='color:yellow;'>WEB DEVELOPMENT TEAM</b></a><br>© 2022 | All Rights Reserved | SRKR Engineering College, Bhimavaram.</div>
+
+<!-- /.copyright -->
+            </div><!-- /.footer-inner -->
+        </div><!-- /.container -->
+    </section><!-- /#footer-bottom -->
+
+</footer>
+<!-- end Footer -->
